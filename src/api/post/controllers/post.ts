b65311c478ad9e0f48d0d0d1eafc2a1a363920d1
Rodrigo <rdg.hora1@gmail.com>
2023-11-20 12:15:12 -0300

    Liberando a rota de count

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,9 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+module.exports = factories.createCoreController("api::post.post", {
+  count(ctx) {
+    var { query } = ctx.request;
+    return strapi.query("api::post.post").count({ where: query });
+  },
+});
